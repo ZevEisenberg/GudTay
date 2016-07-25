cbb7d9772e2533c81a9252f4bd47861f11f35aa0
Zev Eisenberg <zev@zeveisenberg.com>
2016-07-25 19:54:35 -0400

    Naming things.

--- Gud Tay/Gud Tay/RootViewController.swift
+++ Gud Tay/Gud Tay/RootViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  RootViewController.swift
 //  Gud Tay
 //
 //  Created by Zev Eisenberg on 7/24/16.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class RootViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
