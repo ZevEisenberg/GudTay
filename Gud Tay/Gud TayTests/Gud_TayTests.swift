fa534d384860d8dee0e19e75188da2458740a777
Zev Eisenberg <zev@zeveisenberg.com>
2016-07-24 22:40:30 -0400

    Add new Xcode project.

---
+++ Gud Tay/Gud TayTests/Gud_TayTests.swift
@@ -0,0 +1,36 @@
+//
+//  Gud_TayTests.swift
+//  Gud TayTests
+//
+//  Created by Zev Eisenberg on 7/24/16.
+//  Copyright © 2016 Zev Eisenberg. All rights reserved.
+//
+
+import XCTest
+@testable import Gud_Tay
+
+class Gud_TayTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
