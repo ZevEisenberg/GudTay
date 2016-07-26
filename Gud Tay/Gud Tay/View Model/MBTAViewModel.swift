bea9f98aeeaedc4f5c8ffec80d42105a2a60abf9
Zev Eisenberg <zev@zeveisenberg.com>
2016-07-25 22:35:09 -0400

    Basic app structure.

---
+++ Gud Tay/Gud Tay/View Model/MBTAViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  MBTAViewModel.swift
+//  Gud Tay
+//
+//  Created by Zev Eisenberg on 7/25/16.
+//  Copyright © 2016 Zev Eisenberg. All rights reserved.
+//
+
+class MBTAViewModel {
+
+}
---
+++ Gud Tay/Gud Tay/View/MBTAViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MBTAViewController.swift
+//  Gud Tay
+//
+//  Created by Zev Eisenberg on 7/25/16.
+//  Copyright © 2016 Zev Eisenberg. All rights reserved.
+//
+
+import UIKit
+
+class MBTAViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .gray()
+    }
+
+}
