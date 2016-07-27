9be669a6ef2cd625d3ad73944831e93882c8b102
Zev Eisenberg <zev@zeveisenberg.com>
2016-07-26 20:40:35 -0400

    Delete testing boilerplate.

--- Gud Tay/Gud TayUITests/Gud_TayUITests.swift
+++ Gud Tay/Gud TayUITests/Gud_TayUITests.swift
@@ -28,9 +28,4 @@ class GudTayUITests: XCTestCase {
         super.tearDown()
     }
 
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
 }
