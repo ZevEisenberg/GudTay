6e3547f5332e52e3f25c3d3584427cbd99290115
Zev Eisenberg <zev@zeveisenberg.com>
2016-07-27 12:07:51 -0400

    Add BuddyBuild SDK.

--- Gud Tay/Gud Tay/AppDelegate.swift
+++ Gud Tay/Gud Tay/AppDelegate.swift
@@ -15,6 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
 
+        BuddyBuildSDK.setup()
+
         window = UIWindow(frame: UIScreen.main().bounds)
         let rootVC = RootViewController()
         window?.rootViewController = rootVC
