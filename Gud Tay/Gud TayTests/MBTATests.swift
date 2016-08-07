caaa85312a8c74b8d424feacefb2d8c45124f18e
Zev Eisenberg <zev@zeveisenberg.com>
2016-08-07 02:38:37 -0400

    Fix failing tests.

--- Gud Tay/Gud TayTests/MBTATests.swift
+++ Gud Tay/Gud TayTests/MBTATests.swift
@@ -17,32 +17,22 @@ class GudTayTests: XCTestCase {
         let viewModel = MBTAViewModel(serviceType: MockMBTAService.self)
         viewModel.refresh { result in
             switch result {
-            case .success(let tripsAndHeaders):
+            case .success(let trips):
 
-                let subwayOrangeTrips = tripsAndHeaders[0].trips
-                let subwayOrangeHeader = tripsAndHeaders[0].header
+                let subwayOrangeTrips = trips[0]
                 XCTAssertEqual(subwayOrangeTrips, MBTAViewModel.UpcomingTrips.two(next: 243.0, later: 771.0))
-                XCTAssertEqual(subwayOrangeHeader, MBTAViewModel.Header.subway(route: "Orange Line", destination: "Forest Hills"))
 
-                let busCT2Trips = tripsAndHeaders[1].trips
-                let busCT2Header = tripsAndHeaders[1].header
+                let busCT2Trips = trips[1]
                 XCTAssertEqual(busCT2Trips, MBTAViewModel.UpcomingTrips.one(next: 826.0))
-                XCTAssertEqual(busCT2Header, MBTAViewModel.Header.bus(route: "CT2", destination: "Ruggles (Limited Stops)"))
 
-                let bus86Trips = tripsAndHeaders[2].trips
-                let bus86Header = tripsAndHeaders[2].header
+                let bus86Trips = trips[2]
                 XCTAssertEqual(bus86Trips, MBTAViewModel.UpcomingTrips.one(next: 1000.0))
-                XCTAssertEqual(bus86Header, MBTAViewModel.Header.bus(route: "86", destination: "Reservoir"))
 
-                let bus90Trips = tripsAndHeaders[3].trips
-                let bus90Header = tripsAndHeaders[3].header
+                let bus90Trips = trips[3]
                 XCTAssertEqual(bus90Trips, MBTAViewModel.UpcomingTrips.one(next: 659.0))
-                XCTAssertEqual(bus90Header, MBTAViewModel.Header.bus(route: "90", destination: "Wellington"))
 
-                let bus91Trips = tripsAndHeaders[4].trips
-                let bus91Header = tripsAndHeaders[4].header
+                let bus91Trips = trips[4]
                 XCTAssertEqual(bus91Trips, MBTAViewModel.UpcomingTrips.one(next: 1661.0))
-                XCTAssertEqual(bus91Header, MBTAViewModel.Header.bus(route: "91", destination: "Central"))
             case .failure(let error):
                 XCTFail("got unexpected error: \(error)")
             }
