71151e0848bfab955de2676216087a5c4027fdd6
Zev Eisenberg <zev@zeveisenberg.com>
2017-02-25 18:48:12 -0500

    Delete boilerplate.

--- Gud Tay/Gud TayUITests/Gud_TayUITests.swift
+++ Gud Tay/Gud TayUITests/Gud_TayUITests.swift
@@ -36,9 +36,4 @@ class GudTayUITests: XCTestCase {
         XCTAssert(window.exists)
     }
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-
 }
