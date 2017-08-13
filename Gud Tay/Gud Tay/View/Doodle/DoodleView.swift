92388277b0e5139f6246ddcd6723bb94968b346e
Zev Eisenberg <zev@zeveisenberg.com>
2017-08-13 12:20:02 -0400

    Naïve implementation of whiteboard view.

--- Gud Tay/Gud Tay/View/Doodle/DoodleView.swift
+++ Gud Tay/Gud Tay/View/Doodle/DoodleView.swift
@@ -8,8 +8,65 @@
 
 final class DoodleView: GridView {
 
+    // Private Properties
+
+    private var lineColor = UIColor.black
+    private var lineWidth: CGFloat = 10.0
+
+    fileprivate var path = UIBezierPath()
+
     override init(frame: CGRect) {
         super.init(frame: frame)
+
+        let pan = UIPanGestureRecognizer(target: self, action: #selector(panned(sender:)))
+        addGestureRecognizer(pan)
+    }
+
+    override func draw(_ rect: CGRect) {
+        lineColor.setStroke()
+        path.lineWidth = lineWidth
+        path.lineCapStyle = .round
+        path.lineJoinStyle = .round
+        path.stroke()
+    }
+
+}
+
+// MARK: - Actions
+
+private extension DoodleView {
+
+    @objc func panned(sender: UILongPressGestureRecognizer) {
+        let point = sender.location(in: self)
+        switch sender.state {
+        case .began:
+            startAt(point)
+        case .changed:
+            continueTo(point)
+        case .ended, .cancelled:
+            endAt(point)
+        default:
+            fatalError("That's not a thing: \(sender.state)")
+        }
+    }
+
+}
+
+// MARK: - Private
+
+private extension DoodleView {
+
+    func startAt(_ point: CGPoint) {
+        path.move(to: point)
+    }
+
+    func continueTo(_ point: CGPoint) {
+        path.addLine(to: point)
+        setNeedsDisplay()
+    }
+
+    func endAt(_ point: CGPoint) {
+        // no-op
     }
 
 }
