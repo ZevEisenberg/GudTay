bade658157c207c809864829b727992a7028d34c
Zev Eisenberg <zev@zeveisenberg.com>
2017-10-13 13:13:15 -0400

    Update BonMot and Anchorage.

--- Gud Tay/Pods/BonMot/Sources/XMLBuilder.swift
+++ Gud Tay/Pods/BonMot/Sources/XMLBuilder.swift
@@ -46,14 +46,6 @@ extension NSAttributedString {
         return attributedString
     }
 
-    ///
-    /// - parameter xml: The string containing the markup.
-    /// - parameter rules: A protocol to decorate the XML
-    /// - parameter options: XML parsing options
-    ///
-    /// - returns: An NSAttributedString
-    //swiftlint:disable:next valid_docs  (swiftlint issue jpsim/SourceKitten/issues/133)
-
     /// Generate an attributed string by parsing `fragment` using the collection
     /// of `XMLStyleRule`s to provide style and string insertions to decorate
     /// the XML. As the XML fragment is traversed, the style of each node is
@@ -337,7 +329,7 @@ class XMLBuilder: NSObject, XMLParserDelegate {
         currentString = nil
     }
 
-    @objc func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String]) {
+    @objc func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String: String]) {
         foundNewString()
         enter(element: elementName, attributes: attributeDict)
     }
