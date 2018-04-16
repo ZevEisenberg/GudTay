fed54ab074708c32640522e6e7b8dc3018d23cfb
Zev Eisenberg <zev@zeveisenberg.com>
2018-04-16 18:39:13 -0400

    Content view controller.

--- GudTay2/app/GudTay/Screens/ContentViewController.swift
+++ GudTay2/app/GudTay/Screens/ContentViewController.swift
@@ -6,13 +6,49 @@
 //  Copyright © 2017 ZevEisenberg. All rights reserved.
 //
 
+import Anchorage
+import Then
 import UIKit
 
 class ContentViewController: UIViewController {
 
+    private let mbtaViewController: UIViewController //TODO: MBTAViewController
+    private let weatherViewController: UIViewController //TODO: WeatherViewController
+
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+
+        // MBTA
+
+        mbtaViewController = UIViewController()
+
+        // Weather
+
+        weatherViewController = UIViewController()
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+
+    }
+
+    @available(*, unavailable) required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+
+        let mainStackView = UIStackView(axId: "mainStackView").then {
+            $0.axis = .vertical
+            $0.alignment = .fill
+        }
+        view.addSubview(mainStackView)
+        mainStackView.addArrangedSubview(mbtaViewController.view)
+        mainStackView.addArrangedSubview(weatherViewController.view)
+        mainStackView.horizontalAnchors == view.horizontalAnchors
+        mainStackView.verticalAnchors == view.safeAreaLayoutGuide.verticalAnchors
+        weatherViewController.view.heightAnchor == view.heightAnchor * 0.2
+
+        mbtaViewController.view.backgroundColor = Asset.Colors.orange.color
+        weatherViewController.view.backgroundColor = Asset.Colors.day.color
     }
 
 }
