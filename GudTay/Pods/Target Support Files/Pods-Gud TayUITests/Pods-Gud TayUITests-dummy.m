99536637189e9d1eadffc35373e9a9d34c1cbf06
Zev Eisenberg <zev@zeveisenberg.com>
2018-04-19 10:44:03 -0400

    Move Gud Tay 2 to replace Gud Tay 1.

--- GudTay/Pods/Target Support Files/Pods-Gud TayUITests/Pods-Gud TayUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Gud_TayUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Gud_TayUITests
-@end
--- GudTay/Pods/Target Support Files/Swiftilities-Math/Swiftilities-Math-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Swiftilities_Math : NSObject
-@end
-@implementation PodsDummy_Swiftilities_Math
-@end
