078b8a48d6cacb660c77f878f117682159a0864d
Zev Eisenberg <zev@zeveisenberg.com>
2018-05-08 09:30:35 -0400

    Attemp to prevent negative predictions by filtering out predictions earlier than the current time.

--- GudTay/app/GudTay/Screens/MBTA/MBTAViewController.swift
+++ GudTay/app/GudTay/Screens/MBTA/MBTAViewController.swift
@@ -21,6 +21,7 @@ class MBTAViewController: UIViewController {
             let predictionsOfInterest = data.predictions
                 .filter { $0.route.id == APIConstants.routeOfInterest }
                 .filter { $0.directionId == $0.route.directionNames.index(of: "Inbound")! }
+                .filter { $0.departureTime > data.date }
                 .sorted { $0.departureTime < $1.departureTime }
                 .prefix(2)
             busHeader.setUpcomingTrips(upcomingTrips: MBTARouteView.UpcomingTrips(predictions: Array(predictionsOfInterest)), relativeToDate: data.date)
