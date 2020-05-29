2aab511c02049cad6d7e748ccede54d686cf015c
Zev Eisenberg <zev@zeveisenberg.com>
2020-05-29 15:55:53 -0400

    Add additional Mains.

--- GudTay/app/Services/API/Model/OpenWeather/Main.swift
+++ GudTay/app/Services/API/Model/OpenWeather/Main.swift
@@ -8,8 +8,21 @@
 public extension OpenWeatherAPI {
 
     enum Main: String, Decodable {
+        case ash = "Ash"
+        case clear = "Clear"
         case clouds = "Clouds"
+        case drizzle = "Drizzle"
+        case dust = "Dust"
+        case fog = "Fog"
+        case haze = "Haze"
+        case mist = "Mist"
         case rain = "Rain"
+        case sand = "Sand"
+        case smoke = "Smoke"
+        case snow = "Snow"
+        case squall = "Squall"
+        case thunderstorm = "Thunderstorm"
+        case tornado = "Tornado"
     }
 
 }
